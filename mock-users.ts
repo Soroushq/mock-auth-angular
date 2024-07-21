55616fe8df44586551793b4b426d3dc8f2e1f42f
Soroush <166734554+Soroushq@users.noreply.github.com>
2024-07-21 18:33:16 +0330

    Create mock-users.ts

---
+++ mock-users.ts
@@ -0,0 +1,5 @@
+export const mockUsers = [
+  { username: 'user1', password: 'password1', fullName: 'fullname1' },
+  { username: 'user2', password: 'password2', fullName: 'fullname2' },
+  // Add more mock users as needed
+];
